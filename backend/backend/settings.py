5650428fa481977e2e71515b378156480cb25c9e
mhkmcp <mhkmcp@yahoo.com>
2022-01-12 22:23:52 +0600

    backend 008: added Product model and resolve django admin styles

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -87,7 +88,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -126,7 +127,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static_project"),
+# ]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- backend/base/models.py
+++ backend/base/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.fields.related import ForeignKey, OneToOneField 
 
-# Create your models here.
+
+class Product(models.Model):
+    user = ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=255, null=True, blank=True)
+    # image = 
+    brand =  models.CharField(max_length=255, null=True, blank=True)
+    category =  models.CharField(max_length=255, null=True, blank=True)
+    description =  models.TextField()
+    rating =  models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    num_reviews = models.IntegerField(default=0, null=True, blank=True)
+    price =  models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    count_in_stock =  models.IntegerField(default=0, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now =True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+
+    def __str__(self) -> str:
+        return self.name
