0b986889c4e14cbf7077313e2acbae4293b0e59c
mhkmcp <mhkmcp@yahoo.com>
2022-01-12 22:52:06 +0600

    backend 010: migrated models

--- backend/base/models.py
+++ backend/base/models.py
@@ -7,15 +7,16 @@ class Product(models.Model):
     user            = ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name            = models.CharField(max_length=255, null=True, blank=True)
     # image = 
-    brand           =  models.CharField(max_length=255, null=True, blank=True)
-    category        =  models.CharField(max_length=255, null=True, blank=True)
-    description     =  models.TextField()
-    rating          =  models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    brand           = models.CharField(max_length=255, null=True, blank=True)
+    category        = models.CharField(max_length=255, null=True, blank=True)
+    description     = models.TextField()
+    rating          = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     num_reviews     = models.IntegerField(default=0, null=True, blank=True)
-    price           =  models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
-    count_in_stock  =  models.IntegerField(default=0, null=True, blank=True)
+    price           = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    count_in_stock  = models.IntegerField(default=0, null=True, blank=True)
     created_at      = models.DateTimeField(auto_now_add=True)
     updated_at      = models.DateTimeField(auto_now =True)
+    _id             = models.AutoField(primary_key=True, editable=False)
 
 
     def __str__(self) -> str:
@@ -25,9 +26,9 @@ class Product(models.Model):
 class Review(models.Model):
     product         = ForeignKey(Product, on_delete=models.SET_NULL, null=True)
     user            = ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    name            =  models.CharField(max_length=255, null=True, blank=True)
+    name            = models.CharField(max_length=255, null=True, blank=True)
     rating          = models.IntegerField(default=0, null=True, blank=True)
-    comment         =  models.TextField()
+    comment         = models.TextField()
   
     def __str__(self) -> str:
         return self.name + ' ' + str(self.rating)
@@ -40,7 +41,7 @@ class Order(models.Model):
     shipping_price  = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     total_price     = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     is_paid         = models.BooleanField(default=False)
-    paid_at         =  models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    paid_at         = models.DateTimeField(auto_now_add=False, null=True, blank=True)
     is_delivered    = models.BooleanField(default=False)
     delivered_at    = models.DateTimeField(auto_now_add=False, null=True, blank=True)
     created_at      = models.DateTimeField(auto_now_add=True)
@@ -52,7 +53,7 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
     product         = ForeignKey(Product, on_delete=models.SET_NULL, null=True)
-    order           =  ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    order           = ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     name            = models.CharField(max_length=255, null=True, blank=True)
     quantity        = models.IntegerField(default=0, null=True, blank=True)
     price           = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
@@ -61,7 +62,7 @@ class OrderItem(models.Model):
     def __str__(self) -> str:
         return self.name
  
- 
+
 class ShippingAddress(models.Model):
     order           = models.OneToOneField(Order, on_delete=models.CASCADE, null=True, blank=True)
     address         = models.CharField(max_length=255, null=True, blank=True)
